85181cd6e763d4f90151958280608fdaa152f135
vamzee96 <35753721+vamzee96@users.noreply.github.com>
2019-03-19 21:48:03 +0530

    Create letter.py

---
+++ letter.py
@@ -0,0 +1,8 @@
+s=input()
+if (s.isalpha()==True):
+	if(s == 'a' or s == 'e' or s == 'i' or s == 'o' or s == 'u' or s == 'A' or s == 'E' or s == 'I' or s == 'O' or s == 'U'):
+		print("Vowel")
+	else:
+		print("Consonant")
+else:
+	print("invalid")
