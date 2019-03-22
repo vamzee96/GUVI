4ad2caf60a39206d01ac9c5d3a33dd3111b8a525
vamzee96 <35753721+vamzee96@users.noreply.github.com>
2019-03-22 20:48:54 +0530

    Update odderall.py

--- odderall.py
+++ odderall.py
@@ -1,4 +1,7 @@
 a, b= map(int, input().split())
 for num in range(a+1, b):
-    if num % 2 != 0: 
-        print(num, end = " ")
+    if num % 2 != 0:
+        if num==b-1 or num==b-2:
+            print(num)
+        else:
+            print(num, end = " ")
