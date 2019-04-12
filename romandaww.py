22f710ac1b13352033f3ba110beb337913df137e
vamzee96 <35753721+vamzee96@users.noreply.github.com>
2019-04-12 14:37:58 +0530

    Update romandaww.py

--- romandaww.py
+++ romandaww.py
@@ -1,8 +1,8 @@
 romstr=input()
 romdix={'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
 numset = []
-for char in romstr:
-	numset.append(romdix[char]) 
+for i in romstr:
+	numset.append(romdix[i]) 
 value = 0
 for x,y in zip(numset, numset[1:]):
 	if x >= y:
